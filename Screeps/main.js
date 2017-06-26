c939bf68c40f6be6e306292395989d3d3b4f2597
Pumpitx <phenom98@googlemail.com>
2017-06-26 19:25:43 +0200

    initial commit: created repo and added code

---
+++ Screeps/main.js
@@ -0,0 +1,19 @@
+//Import der Module
+var roleHarvester = require("role.Harvester");
+//Endlosschleife ( Ausführung je Tick )
+module.exports.loop = function () {
+
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        // if (creep.memory.role == 'harvester') {
+        //     roleHarvester.run(creep);
+        // }
+        // if (creep.memory.role == 'upgrader') {
+        //     roleUpgrader.run(creep);
+        // }
+        // if (creep.memory.role == "builder") {
+        //     roleBuilder.run(creep);
+        // }
+    }
+
+}
---
+++ Screeps/role.harvester.js
@@ -0,0 +1,29 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function (creep) {
+
+        if (creep.memory.working == true && creep.carry.energy == 0) {
+            creep.memory.working = false;
+        }
+        else (creep.memory.working == false && creep.carry.energy == creep.carry.carryCapacity)
+        {
+            creep.memory.working = true;
+        }
+
+        //Laufen zum Spawn
+        if (creep.memory.working) {
+            if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns.Spawn1);
+            }
+        }
+        else {
+            var source = creep.pos.findClosestByPaath(FIND_SOURCES);
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveto(source);
+            }
+        }
+    }
+};
+
+module.exports = roleHarvester;

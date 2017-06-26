04256014f41115aaf8cda70227ce440013903eb3
Pumpitx <phenom98@googlemail.com>
2017-06-26 20:56:55 +0200

    fixed typo

--- Screeps/role.upgrader.js
+++ Screeps/role.upgrader.js
@@ -7,7 +7,7 @@ var roleUpgrader = {
         if (creep.memory.working == true && creep.carry.energy == 0) {
             creep.memory.working = false;
         }
-        else if (creep.memory.working == false && creep.carry.energy == creep.energyCapacity) {
+        else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
             creep.memory.working = true;
         }
 

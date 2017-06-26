34a8beeedb85793f0c3eb036ae60d855f06e5908
Pumpitx <phenom98@googlemail.com>
2017-06-26 19:48:00 +0200

    changed code

--- Screeps/role.harvester.js
+++ Screeps/role.harvester.js
@@ -13,14 +13,14 @@ var roleHarvester = {
 
         //Laufen zum Spawn
         if (creep.memory.working) {
-            if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns.Spawn1);
+            if (creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns["Spawn1"]);
             }
         }
         else {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveto(source);
+            var source = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(source[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveto(source[0]);
             }
         }
     }

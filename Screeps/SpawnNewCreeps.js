1c8daf39fecaaf11513117876d1ae2edbcfd6359
Pumpitx <phenom98@googlemail.com>
2017-06-29 19:28:29 +0200

    s

--- Screeps/SpawnNewCreeps.js
+++ Screeps/SpawnNewCreeps.js
@@ -3,7 +3,7 @@ var CheckCreeps = {
     run: function(role) {
     var creeps = _.sum(Game.creeps, (c) => c.memory.role == role);
     
-    if (creeps < 2) {
+    if (creeps < 3) {
         if (Game.spawns["Spawn1"].energy > 200) {
             Game.spawns["Spawn1"].createCreep([MOVE,CARRY,WORK],undefined,{role: role,working: false})
             }

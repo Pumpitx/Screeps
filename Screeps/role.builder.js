83593fbc6d6a29962a3176bb643daccc45474183
Pumpitx <phenom98@googlemail.com>
2017-06-29 19:26:02 +0200

    added Klammer

--- Screeps/role.builder.js
+++ Screeps/role.builder.js
@@ -11,12 +11,12 @@ var roleBuilder = {
 
         if (creep.memory.working) {
             var constructionSite = creep.pos.findClostestByPath(FIND_CONSTRUCTION_SITES);
-            if (creep.build((constructionSite) == ERR_NOT_IN_RANGE) {
+            if (creep.build((constructionSite) == ERR_NOT_IN_RANGE)) {
                 creep.moveTo(constructionSite);
             }
         }
         else {
-            var source = creep.pos.findClostestByPath(FIND_SOURCES) {
+            var source = creep.pos.findClostestByPath(FIND_SOURCES);
                 if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(source);
                 }

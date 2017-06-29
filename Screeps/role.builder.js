c33f4852500c2b7b725c78bbe1ea6cb394ce8503
Pumpitx <phenom98@googlemail.com>
2017-06-29 20:18:48 +0200

    fixed typo

--- Screeps/role.builder.js
+++ Screeps/role.builder.js
@@ -10,13 +10,13 @@ var roleBuilder = {
         }
 
         if (creep.memory.working) {
-            var constructionSite = creep.pos.findClostestByPath(FIND_CONSTRUCTION_SITES);
+            var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
             if (creep.build((constructionSite) == ERR_NOT_IN_RANGE)) {
                 creep.moveTo(constructionSite);
             }
         }
         else {
-            var source = creep.pos.findClostestByPath(FIND_SOURCES);
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
                 if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(source);
                 }
